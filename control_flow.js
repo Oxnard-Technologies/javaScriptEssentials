7f13021aeddbb0b43120b63bd65efbcfe392523d
RupinderKaur16 <rukaur@oxnardtechnologies.com>
2024-08-08 01:17:58 -0400

    Control flow and conditional statements

---
+++ control_flow.js
@@ -0,0 +1,57 @@
+//if & else if statements
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access level: " + accessLevel);
+
+//nested if statment
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+console.log("User message: " + userMessage);
+
+//switch statement
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User category: " + userCategory);
+
+//Ternary operator
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "authenticated" : "not authenticated";
+
+console.log("Authentication status: " + authenticationStatus);
---
+++ scope_lab2.js
@@ -0,0 +1,17 @@
+{
+    var blockVar = "I am a block var variable.";
+    //let blockLet = "I am a block let variable.";
+    //const blockConst = "I am a block const variable.";
+
+    //blockVar = "I am reassigned var variable in block.";
+    //blockLet = "I am reassigned let variable in block.";
+    //blockConst = "I am reassigned const variable in block.";
+}
+
+    blockVar = "I am reassigned var variable in block.";
+    blockLet = "I am reassigned let variable in block.";
+    blockConst = "I am reassigned const variable in block.";
+
+    console.log(blockVar);
+    console.log(blockLet);
+    console.log(blockConst);
