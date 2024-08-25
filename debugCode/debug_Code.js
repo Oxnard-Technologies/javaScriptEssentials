82959d84a4439883d6e6a67ac53d9adc5976895d
RupinderKaur16 <rukaur@oxnardtechnologies.com>
2024-08-25 01:03:10 -0400

    Perform arithmatic operations simultanteously

--- debugCode/debug_Code.js
+++ debugCode/debug_Code.js
@@ -2,23 +2,35 @@
 function performOperation() {
     let input1 = parseInt(document.getElementById('input1').value);
     let input2 = parseInt(document.getElementById('input2').value);
+    const resultElement = document.getElementById('result');
+
+    debugger;
 
     if(!isNaN(input1) && !isNaN(input2)) {
-        let result = multiply(input1, input2);
-        displayResult(result);
-    } else {
-        displayResult('Please enter valid numbers.');
+        let mul = multiply(input1, input2);
+        let addition = add(input1, input2);
+        let divi = division(input1, input2);
+
+        let result = ("" + addition + ", " + mul + ", " + divi);
+
+        resultElement.textContent = 'The addtion, multiplication, and division results are ' + result + ' respectively.';
+
+        //displayResult(result);
+
+    }else {
+        //displayResult('Please enter valid numbers.');
+        resultElement.textContent = 'Please enter valid numbers.';
     }
 }    
 
 function multiply(a, b) {
-
-    debugger;
-
     return a * b;
 }
-        
-function displayResult(result) {
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = 'The result is: ' + result;
+
+function add(a, b) {
+    return a + b;
+}
+
+function division(a, b) {
+    return a / b;
 }
