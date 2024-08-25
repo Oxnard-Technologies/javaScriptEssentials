b711423eaaba9590a2960c40af6bc4643836768d
RupinderKaur16 <rukaur@oxnardtechnologies.com>
2024-08-24 23:37:18 -0400

    Code Debugging Example

---
+++ debugCode/debug_Code.js
@@ -0,0 +1,24 @@
+    
+function performOperation() {
+    let input1 = parseInt(document.getElementById('input1').value);
+    let input2 = parseInt(document.getElementById('input2').value);
+
+    if(!isNaN(input1) && !isNaN(input2)) {
+        let result = multiply(input1, input2);
+        displayResult(result);
+    } else {
+        displayResult('Please enter valid numbers.');
+    }
+}    
+
+function multiply(a, b) {
+
+    debugger;
+
+    return a * b;
+}
+        
+function displayResult(result) {
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = 'The result is: ' + result;
+}
